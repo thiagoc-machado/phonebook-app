c91acb0f7e648aa04ca5e35d4d73635af003da88
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-02 00:34:44 +0100

    EditContacts

--- frontend/src/components/contacts/EditContact.jsx
+++ frontend/src/components/contacts/EditContact.jsx
@@ -12,66 +12,71 @@ let EditContact = () => {
               <p className='fst-italic'>Loren</p>
             </div>
           </div>
-          <div className='row'>
-            <form>
-              <div className='mb-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Name'
-                />
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='number'
-                  className='form-control'
-                  placeholder='Phone'
-                />
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='number'
-                  className='form-control'
-                  placeholder='Mobile'
-                />
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='email'
-                  className='form-control'
-                  placeholder='Email'
-                />
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Company'
-                />
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Title'
-                />
-              </div>
-              <div className='mb-2'>
-                <select className='form-control'>
-                  <option value=''>Select a Group</option>
-                </select>
-              </div>
-              <div className='mb-2'>
-                <input
-                  type='submit'
-                  className='btn btn-primary'
-                  value='Update'
-                />
-                <Link to={'/contacts/list'} className='btn btn-dark ms-2'>
-                  Cancel
-                </Link>
-              </div>
-            </form>
+          <div className='row align-items-center'>
+            <div className='col-md-4'>
+              <form>
+                <div className='mb-2'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    placeholder='Name'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='number'
+                    className='form-control'
+                    placeholder='Phone'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='number'
+                    className='form-control'
+                    placeholder='Mobile'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='email'
+                    className='form-control'
+                    placeholder='Email'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    placeholder='Company'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    placeholder='Title'
+                  />
+                </div>
+                <div className='mb-2'>
+                  <select className='form-control'>
+                    <option value=''>Select a Group</option>
+                  </select>
+                </div>
+                <div className='mb-2'>
+                  <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Update'
+                  />
+                  <Link to={'/contacts/list'} className='btn btn-dark ms-2'>
+                    Cancel
+                  </Link>
+                </div>
+              </form>
+            </div>
+            <div className='col-md-6'>
+                <img src='https://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png' className='contact-img'/>
+            </div>
           </div>
         </div>
       </section>
