ec4ff101582eb037d5451c558146451190aefd07
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-02 14:10:36 +0100

    addContact

--- frontend/src/components/contacts/AddContact.jsx
+++ frontend/src/components/contacts/AddContact.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ContactService } from '../../services/ContactService';
 
 let AddContact = () => {
+  let navigate = useNavigate();
+
+  let [state, setState] = useState({
+    loading: false,
+    contact: {
+      name: '',
+      phone: '',
+      mobile: '',
+      email: '',
+      company: '',
+      title: '',
+      groupId: '',
+    },
+    groups: [],
+    errorMessage: '',
+  });
+
+  let updateInput = (event) => {
+    setState({
+      ...state,
+      contact: {
+        ...state.contact,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setState({ ...state, loading: true });
+        let response = await ContactService.getGroups();
+        setState({
+          ...state,
+          loading: false,
+          groups: response.data,
+        });
+      } catch (error) {}
+    }
+    fetchData();
+  }, []);
+
+  let submitForm = async (event) => {
+    event.preventDefault();
+    try {
+      let response = await ContactService.createContact(state.contact);
+      if (response) {
+        navigate('/contacts/list', { replace: true });
+      }
+    } catch (error) {
+      setState({ ...state, errorMessage: error.message });
+      navigate('/contacts/add', { replace: false });
+    }
+  };
+
+  let { loading, contact, groups, errorMessage } = state;
+
   return (
     <React.Fragment>
       <section className='add-contact p-3'>
@@ -14,9 +72,13 @@ let AddContact = () => {
           </div>
           <div className='row'>
             <div className='col-md-4'>
-              <form>
+              <form onSubmit={submitForm}>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='name'
+                    value={contact.name}
+                    onChange={updateInput}
                     type='text'
                     className='form-control'
                     placeholder='Name'
@@ -24,6 +86,10 @@ let AddContact = () => {
                 </div>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='phone'
+                    value={contact.phone}
+                    onChange={updateInput}
                     type='number'
                     className='form-control'
                     placeholder='Phone'
@@ -31,6 +97,10 @@ let AddContact = () => {
                 </div>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='mobile'
+                    value={contact.mobile}
+                    onChange={updateInput}
                     type='number'
                     className='form-control'
                     placeholder='Mobile'
@@ -38,6 +108,10 @@ let AddContact = () => {
                 </div>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='email'
+                    value={contact.email}
+                    onChange={updateInput}
                     type='email'
                     className='form-control'
                     placeholder='Email'
@@ -45,6 +119,10 @@ let AddContact = () => {
                 </div>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='company'
+                    value={contact.company}
+                    onChange={updateInput}
                     type='text'
                     className='form-control'
                     placeholder='Company'
@@ -52,14 +130,32 @@ let AddContact = () => {
                 </div>
                 <div className='mb-2'>
                   <input
+                    required={true}
+                    name='title'
+                    value={contact.title}
+                    onChange={updateInput}
                     type='text'
                     className='form-control'
                     placeholder='Title'
                   />
                 </div>
                 <div className='mb-2'>
-                  <select className='form-control'>
+                  <select
+                    required={true}
+                    name='groupId'
+                    value={contact.groupId}
+                    onChange={updateInput}
+                    className='form-control'
+                  >
                     <option value=''>Select a Group</option>
+                    {groups.length > 0 &&
+                      groups.map((group) => {
+                        return (
+                          <option key={group.id} value={group.id}>
+                            {group.name}
+                          </option>
+                        );
+                      })}
                   </select>
                 </div>
                 <div className='mb-2'>
--- frontend/src/services/ContactService.jsx
+++ frontend/src/services/ContactService.jsx
@@ -2,23 +2,28 @@ import axios from 'axios';
 export class ContactService {
   static serverURL = 'http://localhost:9000';
 
-  static getGroups(){
+  static getGroups() {
     let dataURL = `${this.serverURL}/groups`;
-    return axios.get(dataURL)
+    return axios.get(dataURL);
   }
 
-  static getGroup(contact){
+  static getGroup(contact) {
     let groupId = contact.groupId;
     let dataURL = `${this.serverURL}/groups/${groupId}`;
-    return axios.get(dataURL)
+    return axios.get(dataURL);
   }
 
   static getAllContacts() {
     let dataURL = `${this.serverURL}/contacts`;
     return axios.get(dataURL);
   }
-  static getContact(contactId){
+  static getContact(contactId) {
     let dataURL = `${this.serverURL}/contacts/${contactId}`;
-    return axios.get(dataURL)
+    return axios.get(dataURL);
+  }
+
+  static createContact(contact) {
+    let dataURL = `${this.serverURL}/contacts`;
+    return axios.post(dataURL, contact);
   }
 }
