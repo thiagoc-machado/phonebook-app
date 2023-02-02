fb7b9a04c608e0bd9ee60e7b44b82d347eddabce
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-03 00:37:02 +0100

    users

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def user_View(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
