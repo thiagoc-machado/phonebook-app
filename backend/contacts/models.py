c4c21dd07071c75b0c056e992ce69a509fac119c
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-02-05 02:30:22 +0100

    hash password

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 class contacts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15, null=True, blank=True)
     mobile = models.CharField(max_length=15, null=True, blank=True)
@@ -8,14 +11,13 @@ class contacts(models.Model):
     company = models.CharField(max_length=30, null=True, blank=True)
     title = models.CharField(max_length=20, null=True, blank=True)
     groupId = models.CharField(max_length=10, null=True, blank=True)
-    #id = models.CharField(max_length=14, null=True, blank=True)
     addDate = models.DateField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class groups(models.Model):
-    #id = models.CharField(max_length=50)
     name = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,24 +3,18 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from users.serializers import UserSerializer
 from rest_framework import status
+from django.contrib.auth.hashers import make_password
 
-
-
-
-# @api_view(['GET','POST'])
-# def user_View(request):
-#     serializer = UserSerializer(request.user)
-#     return Response(serializer.data)
-
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def user_View(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            password = request.data['password']
+            serializer.validated_data['password'] = make_password(password)
+            user = serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
-
